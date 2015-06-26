825dffe68635e6903256da6e99ed5638d41047ea
ThePheonixBlader <thepheonixblader@gmail.com>
2015-06-26 15:46:38 -0700

    Create Command_sys.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
@@ -0,0 +1,93 @@
+//FOPM's System Administration Command
+//Credit to FOPM Developers
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "System Administration Management", usage = "/<command> <saadd | sadelete> <username>")
+public class Command_sys extends TFM_Command
+{
+
+    @Override
+    public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+
+        if (!KOM_Util.OWNERS.contains(sender.getName())
+        if (!KOM_Util.SYSTEMADMINS.contains(sender.getName())
+        {
+            sender.sendMessage(TotalFreedomMod.MSG_NO_PERMS);
+            TFM_Util.adminAction("WARNING: " + sender.getName(), "Has attempted to use a system admin only command. System administration team has been alerted.", true);
+
+            if (!senderIsConsole)
+            {
+                sender.setOp(false);
+            }
+            else
+            {
+                sender.sendMessage("You are not a System Admin and may NOT use this command. If you feel this in error please contact a Developer.");
+            }
+
+            return true;
+        }
+
+        if (args.length == 0)
+        {
+            return false;
+        }
+        else if (args.length == 1)
+        {
+            return false;
+        }
+
+        else if (args.length == 2)
+        {
+            if (args[0].equalsIgnoreCase("saadd"))
+            {
+                Player player = null;
+                String playername = null;
+
+                player = getPlayer(args[1]);
+
+                if (player != null)
+                {
+                    TFM_Util.adminAction(sender.getName(), "Adding " + player.getName() + " to the superadmin list.", true);
+                    TFM_AdminList.addSuperadmin(player);
+                }
+                else if (playername != null)
+                {
+                    TFM_Util.adminAction(sender.getName(), "Adding " + playername + " to the superadmin list.", true);
+                    TFM_AdminList.addSuperadmin(player);
+                }
+                return true;
+            }
+
+            else if (args[0].equalsIgnoreCase("sadelete") || args[0].equalsIgnoreCase("del") || args[0].equalsIgnoreCase("remove"))
+            {
+
+                String targetName = args[1];
+
+                targetName = getPlayer(targetName).getName();
+
+                if (!TFM_AdminList.getLowerSuperNames().contains(targetName.toLowerCase()))
+                {
+                    playerMsg("Superadmin not found: " + targetName);
+                    return true;
+                }
+
+                TFM_Util.adminAction(sender.getName(), "Removing " + targetName + " from the superadmin list", true);
+                TFM_AdminList.removeSuperadmin(Bukkit.getOfflinePlayer(targetName));
+
+                return true;
+            }
+            return true;
+        }
+        return true;
+    }
+}
