bf3f6bc8c3b5f7f46b333c44f256fdd09008b390
ThePheonixBlader <thepheonixblader@gmail.com>
2015-06-26 16:19:16 -0700

    Create AdminLevel.java

---
+++ src/com/Cyro1999/KrazyOPMod/Commands/AdminLevel.java
@@ -0,0 +1,18 @@
+
+package com.Cyro1999.KrazyOPMod.Commands;
+
+public enum AdminLevel
+{
+    ALL("All Player Commands"), OP("OP Commands"), SUPER("SuperAdmin Commands"), SENIOR("Senior Admin Commands");
+    //
+    private final String friendlyName;
+
+    private AdminLevel(String friendlyName)
+    {
+        this.friendlyName = friendlyName;
+    }
+
+    public String getFriendlyName()
+    {
+        return friendlyName;
+    }
